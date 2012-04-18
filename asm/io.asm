4bd84548934c25bb594599cd5d1cf0871d92f254
Alexios Chouchoulas <alexios@bedroomlan.org>
2012-04-18 22:15:28 +0000

    Minor changes to support updated hardware.

--- asm/io.asm
+++ asm/io.asm
@@ -1,10 +1,11 @@
 ;;; Some standard I/O mnemonics
 
-.equ PANEL   &0000
-.equ IPSW    IN PANEL 0		; Read panel switches
-.equ OPLT    OUT PANEL 1	; Write panel lights
-.equ IHWF    IN PANEL 2		; Read hardware flags
-.equ HALT    OUT PANEL 7	; Halt the clock
+.equ PANEL   R &008
+.equ LSR    IN PANEL 0		; r- Load Switch Register (load front panel switches)
+.equ LDSR   IN PANEL 1		; r- Load DIP-Switch Register
+.equ SOR    OUT PANEL 2		; -w Set Output Register (set panel lights)
+.equ HALT   OUT PANEL 7		; -w Halt the clock
+	
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -12,15 +13,14 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-.equ TTY0 &080
-.equ TTY1 &084
-.equ TTY2 &088
-.equ TTY3 &08c
+.equ TTY0 &060
+.equ TTY1 &068
+.equ TTY2 &070
+.equ TTY3 &078
 
-.equ TTYPOLL   &0
-.equ TTYDATA   &1
-.equ TTYCTL    &2
-
+.equ TTYSR     &1
+.equ TTYFIFO   &3
+	
 ;;; Interrupt control: OUT 1 to TTYCTL port (OUT 0 to disable
 ;;; interrupts). An interrupt is generated whenever an input byte is
 ;;; available.
@@ -31,5 +31,14 @@
 ;;; returns zero for bits 8-15).
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; The Timer/NVRAM/RTC board
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.equ NVRAMWIN &090
+.equ NVRAMOFS &080
 
 ;;; End of file.
