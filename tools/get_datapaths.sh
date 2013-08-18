f363f7b08ce70ebb9c1002b6c95010a402febc15
Alexios Chouchoulas <alexios@bedroomlan.org>
2013-08-18 20:50:42 +0000

    Created new trunk.

--- tools/get_datapaths.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-./emu.py a.obj | grep LATCH | sort | uniq |
-    sed -e 's/^.*LATCH //' -e 's/ <- /\"->\"/' -e 's/^/\"/' -e 's/$/\"/' \
-	-e 's/PR_UPDATE (//' -e 's/ /_/g' -e 's/->/ -> /' |
-    gawk -- '{ print $3, $2, $1; }'
-
-# End of file.
