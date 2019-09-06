c6b54f1e241cf4109b6466ae2012e3a33c70dc06
Alexios Chouchoulas <alexios@bedroomlan.org>
2019-09-06 15:57:42 +0300

    Working on 138 debugging

--- verilog/demux.v
+++ verilog/demux.v
@@ -49,7 +49,7 @@ module demux_138 (g1, g2a, g2b, a, y);
    // during transitions of A while G1, G2A and G2B remained
    // constant. The hardware '138 doesn't produce these, so this is a
    // bug in this code. Removing delays for now.
-   assign #delay y = (g1 && !g2a && !g2b) ? y0 : 8'b11111111;
+   assign y = (g1 && !g2a && !g2b) ? y0 : 8'b11111111;
    
    always @ (a) begin
       case (a)
