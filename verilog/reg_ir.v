ef2fc8e407174ed4707615d32c5d0c61c715ae87
Alexios Chouchoulas <alexios@bedroomlan.org>
2019-09-28 19:46:30 +0300

    Added IR testbench

--- verilog/reg_ir.v
+++ verilog/reg_ir.v
@@ -20,7 +20,7 @@ module reg_ir (nreset, ibus, nwrite_ir, ir,
 
    input 	 nreset;
    inout [15:0]  ibus;
-   input 	 nwrite_ir,
+   input 	 nwrite_ir;
    output [15:0] ir;
 
    input 	 nfpirl;
@@ -32,11 +32,15 @@ module reg_ir (nreset, ibus, nwrite_ir, ir,
 
    // The original CFT used latches and worked. We'll move to flip-flops
    // here. It should make no difference, but might protect us from some
-   // glitches. It also saves one NOT gate, since latch-enable signals are
-   // active high.
+   // glitches.
 
-   flipflop_573 ir_lo (ibus[7:0],  ir[7:0],  nwrite_ir, nreset);
-   flipflop_573 ir_hi (ibus[15:8], ir[15:8], nwrite_ir, nreset);
+   wire 	 reset;
+   assign #6     reset = ~nreset;
+
+   // latch_573 ir_lo (ibus[7:0],  ir[7:0],  nwrite_ir, nreset);
+   // latch_573 ir_hi (ibus[15:8], ir[15:8], nwrite_ir, nreset);
+   flipflop_574 ir_lo (.d(ibus[7:0]),   .q(ir[7:0]),  .clk(nwrite_ir), .oe(reset));
+   flipflop_574 ir_hi (.d(ibus[15:8]),  .q(ir[15:8]), .clk(nwrite_ir), .oe(reset));
 
    // The front panel buffers
    buffer_541 ir_fpbuf_lo (.a(ir[7:0]),  .y(fpd), .oe1(nfpirl), .oe2(1'b0));
---
+++ verilog/reg_ir_tb.v
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// TEST THE INSTRUCTION REGISTER
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// REDESIGNED IN 2019
+
+`include "reg_ir.v"
+`timescale 1ns/10ps
+
+module reg_ir_tb();
+   reg         nreset;
+   reg         nwrite_ir;
+   reg [15:0]  ibus;
+   wire [15:0] ibus_real;
+   
+   reg 	       nfpirl;
+   reg 	       nfpirh;
+   wire [7:0]  fpd;
+
+   wire [15:0] ir;
+   
+   integer    i;
+   
+   // Initialize all variables
+   initial begin
+      $monitor ("%d %b %b %h %h %b %b %h", $time, nreset, nwrite_ir, ibus_real, ir, nfpirl, nfpirh, fpd);
+      $dumpfile ("vcd/reg_ir_tb.vcd");
+      $dumpvars (0, reg_ir_tb);
+
+      ibus = 16'h0000;
+      nreset = 0;
+      nfpirl = 1;
+      nfpirh = 1;
+      nwrite_ir = 1;
+
+      // Simulate reset
+      #250 nreset = 1;
+
+      for (i = 0; i < 65536; i = i + 1) begin
+	 #250 ibus = i;
+      end
+      
+      #1000 $finish;      // Terminate simulation
+   end // initial begin
+
+   // Load the IR every now and then
+   always begin
+      #1000 nwrite_ir = 0;
+      #63.5 nwrite_ir = 1;
+   end
+
+   assign ibus_real = ibus;
+
+   // Asynchronous Front Panel functionality
+   always begin
+      #1937 nfpirl = 0;		// 1937 is prime, and so is 1937+30+30
+      #30 nfpirl = 1; 
+      nfpirh = 0;
+      #30 nfpirh = 1;
+   end
+
+   // Connect DUT to test bench
+   reg_ir reg_ir (.nreset(nreset),
+		  .ibus(ibus_real),
+		  .nwrite_ir(nwrite_ir),
+		  .ir(ir),
+		  .nfpirl(nfpirl), .nfpirh(nfpirh), .fpd(fpd));
+
+endmodule // reg_ir_tb
+
+// End of file.
