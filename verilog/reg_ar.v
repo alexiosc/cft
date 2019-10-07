af5ddbc7f216300a969f5ebd12c0758fb107d9d9
Alexios Chouchoulas <alexios@bedroomlan.org>
2019-10-08 00:06:00 +0300

    Completed testbench and fixed hazard.

--- verilog/reg_ar.v
+++ verilog/reg_ar.v
@@ -19,8 +19,10 @@
 // This models both the AR register and the I/O address decoder.
 module reg_ar (nmem, nio, ibus, aext, nwrite_ar, ab,
 	       nsysdev, niodev1xx, niodev2xx, niodev3xx,
-	       nfparl, nfparm, nfparh, fpd);
+	       nfparh, fpd);
 
+   input         nmem;
+   input 	 nio;
    inout [15:0]  ibus;
    input [7:0] 	 aext;
    input 	 nwrite_ar;
@@ -40,14 +42,14 @@ module reg_ar (nmem, nio, ibus, aext, nwrite_ar, ab,
    wire 	 naben;
 
    // Drive the Address Bus during MEM and IO cycles.
-   assign #6 naben = nmem & nio;
+   assign #7 naben = nmem & nio;
 
    // The Address Register itself. The '574 has tri-state outputs, but we
    // output the AR to both the Address Bus and the Front Panel, so we '574 FFs
    // and separate buffers.
-   flipflop_574 ar_lo  (.d(ibus[7:0]),   .q(ir[7:0]),   .clk(nwrite_ar), .oe(1'b0));
-   flipflop_574 ar_mid (.d(ibus[15:8]),  .q(ir[15:8]),  .clk(nwrite_ar), .oe(1'b0));
-   flipflop_574 ar_hi  (.d(ibus[23:16]), .q(ir[23:16]), .clk(nwrite_ar), .oe(1'b0));
+   flipflop_574 ar_lo  (.d(ibus[7:0]),   .q(ar[7:0]),   .clk(nwrite_ar), .noe(1'b0));
+   flipflop_574 ar_mid (.d(ibus[15:8]),  .q(ar[15:8]),  .clk(nwrite_ar), .noe(1'b0));
+   flipflop_574 ar_hi  (.d(aext),        .q(ar[23:16]), .clk(nwrite_ar), .noe(1'b0));
 
    // The Address Bus buffers
    buffer_541 ar_ab_lo  (.a(ar[7:0]),   .y(ab[7:0]),   .oe1(naben), .oe2(1'b0));
@@ -59,18 +61,24 @@ module reg_ar (nmem, nio, ibus, aext, nwrite_ar, ab,
    // the FPD bus for the front panel, so only one buffer is needed.
    buffer_541 ar_fpbuf_hi  (.a(ar[23:16]), .y(fpd),   .oe1(nfparh), .oe2(1'b0));
 
-   // The I/O Address Decoder isn't really part of the AR, but they've been
-   // drawn together in schematics for a while, so I'll leave it here. The
-   // decoder provides four convenient control lines to decode the MSB of I/O
-   // space addresses, so devices can usually implement full address decoding
-   // with just a '138 decoder.
+   // The I/O Address Decoder isn't really part of the AR, but if
+   // they're colocated it saves having to bus 8 lines. (AR[15:8])
+   // across units. The decoder provides four convenient control lines
+   // to decode the MSB of I/O space addresses, so devices can usually
+   // implement full address decoding with just a '138 decoder. It
+   // uses the AR for decoding rather than the Address Bus itself so
+   // it can generate a device select strobe as soon as early as
+   // possible.
 
-   wire 	 nar11_15_eq_0;
+   wire 	 nar_high_0;
    wire [7:0] 	 y;
+
+   // Note: AR[10] is decoded TWICE here, to avoid a hazard that can cause spurious nsysdev strobes.
    
-   comparator_688 ioad_cmp (.a({ar[15:11], 3'b000}), .b(8'd0), .ng(1'b0),
-			    .neq(nar11_15_eq_0));
-   demux_138 (.g1(1'b1), .g2a(nio), .g2b(nar11_15_eq_0), .a(ar[10:8]), .y(y));
+   comparator_688 ioad_cmp (.a({ar[15:10], 2'b00}), .b(8'd0), .ng(naben),
+			    .neq(nar_high_0));
+   demux_138 ioad_dec (.g1(1'b1), .g2a(nio), .g2b(nar_high_0),
+		       .a({ar[10:8]}), .y(y));
    assign nsysdev = y[0];
    assign niodev1xx = y[1];
    assign niodev2xx = y[2];
---
+++ verilog/reg_ar_tb.v
@@ -0,0 +1,108 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// TEST THE INSTRUCTION REGISTER
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// REDESIGNED IN 2019
+
+`include "reg_ar.v"
+`timescale 1ns/10ps
+
+module reg_ar_tb();
+   reg          nmem;
+   reg          nio;
+   reg [7:0]    aext;
+   reg [15:0]   ibus;
+   wire [15:0] 	ibus_real;
+
+   wire [23:0] 	ab;
+
+   reg 		nwrite_ar;
+   reg 		nfparh;
+   wire [7:0] 	fpd;
+
+   wire 	nsysdev;
+   wire 	niodev1xx;
+   wire 	niodev2xx;
+   wire 	niodev3xx;
+   
+   integer    i;
+   
+   // Initialize all variables
+   initial begin
+      $monitor ("%d | %b %b %h:%h > %h %b %b %b %b | %b > %h", $time,
+		nmem, nio, aext, ibus, nwrite_ar,
+		ab, nsysdev, niodev1xx, niodev2xx, niodev3xx,
+		nfparh, fpd);
+      $dumpfile ("vcd/reg_ar_tb.vcd");
+      $dumpvars (0, reg_ar_tb);
+
+      aext = 8'd0;
+      ibus = 16'h0000;
+      nwrite_ar = 1;
+      nmem = 1;
+      nfparh = 1;
+
+      // Simulate reset
+      //#250 nreset = 1;
+
+      // First, test I/O transactions because they're easier. We only
+      // have 1,024 I/O addresses, but test up to 2,048. NOTE: there's
+      // a '138 glitch that causes a spurious nsysdev strobe 
+      for (i = 0; i < 2048; i = i + 37) begin
+	 #62.5 ibus = i[15:0];
+	 aext = 8'd0;
+	 nwrite_ar = 0;
+	 #62.5 nwrite_ar = 1;
+	 nio = 0;
+	 #125 nio = 1;
+      end
+
+      #2000;
+      
+      // Now to some of the memory. We'll just do 256K in big
+      // increments. Should be enough!
+      for (i = 0; i < 262144; i = i + 3169) begin
+	 #62.5 ibus = i[15:0];
+	 aext = i[23:16];
+	 nwrite_ar = 0;
+	 #62.5 nwrite_ar = 1;
+	 nmem = 0;
+	 #125 nmem = 1;
+      end
+      
+      #2000 $finish;      // Terminate simulation
+   end // initial begin
+
+   // Load the IR every now and then
+   // always begin
+   //    #1000 nwrite_ir = 0;
+   //    #63.5 nwrite_ir = 1;
+   // end
+
+   assign ibus_real = ibus;
+
+   // Asynchronous Front Panel functionality
+   always begin
+      #229 nfparh = 0;		// 1937 is prime, and so is 1937+30+30
+      #250 nfparh = 1;
+   end
+
+   // Connect DUT to test bench
+   reg_ar reg_ar (.nmem(nmem),
+		  .nio(nio),
+		  .ibus(ibus_real),
+		  .aext(aext),
+		  .nwrite_ar(nwrite_ar),
+		  .ab(ab),
+		  .nsysdev(nsysdev),
+		  .niodev1xx(niodev1xx),
+		  .niodev2xx(niodev2xx),
+		  .niodev3xx(niodev3xx),
+		  .nfparh(nfparh),
+		  .fpd(fpd));
+
+endmodule // reg_ar_tb
+
+// End of file.
