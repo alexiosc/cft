ed78caa95ed45955c3288c08177e87f44a571548
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-02 10:50:42 +0200

    Updated signal names in ALU ROM testbench.

--- verilog/alu_rom_tb.v
+++ verilog/alu_rom_tb.v
@@ -43,15 +43,15 @@
 
 module alu_rom_tb();
 
-   reg            noe;
-   reg 		  flin;
-   reg 		  xin;
+   reg            nromoe;
+   reg 		  fl;
+   reg 		  x_in;
    reg [2:0] 	  op;
    reg [15:0] 	  a, b;
    
    wire [15:0] 	  ibus;
-   wire 	  fvout, nsetv;
-   wire 	  flout, nsetl;
+   wire 	  fvout_rom, nsetv_rom;
+   wire 	  flout_rom, nsetl_rom;
    
    reg [15:0] 	  ibus_drv, db_drv;
    
@@ -75,13 +75,14 @@ module alu_rom_tb();
    initial begin        
       $dumpfile ("vcd/alu_rom_tb.vcd");
       $dumpvars (0, alu_rom_tb);
-      $monitor ("t: %7d | %b %04x (%016b) %04x (%016b) %03b %03s > %b %b %b %b %04x (%016b)",
-		$time,
-		flin, a, a, b, b, op, curop, nsetv, fvout, nsetl, flout, ibus, ibus);
+      // Disabled because this test produces insane amounts of output.
+      // $monitor ("t: %7d | %b %04x (%016b) %04x (%016b) %03b %03s > %b %b %b %b %04x (%016b)",
+      // 		$time,
+      // 		fl, a, a, b, b, op, curop, nsetv_rom, fvout_rom, nsetl_rom, flout_rom, ibus, ibus);
 
-      noe = 1;
-      flin = 1;
-      xin = 0;
+      nromoe = 1;
+      fl = 1;
+      x_in = 0;
       op = 0;
       a = 0;
       b = 0;
@@ -91,24 +92,24 @@ module alu_rom_tb();
       
       for (k = 0; k < 8; k += 1) begin
 	 #5000 op = k;
-	 flin = 0;
+	 fl = 0;
 
 	 da = `DELTA_A;
 	 db = `DELTA_B;
 
 	 #5000 for (j = 0; j <= 65535; j += db) begin
 	    for (i = 0; i <= 65535; i += da) begin
-	       flin = 0;
-	       #50 noe = 0;
+	       fl = 0;
+	       #50 nromoe = 0;
 	       a = i;
 	       b = j;
-	       #450 noe = 1;
+	       #450 nromoe = 1;
 
-	       flin = 1;
-	       #50 noe = 0;
+	       fl = 1;
+	       #50 nromoe = 0;
 	       a = i;
 	       b = j;
-	       #450 noe = 1;
+	       #450 nromoe = 1;
 	    end
 	 end
       end
@@ -117,12 +118,12 @@ module alu_rom_tb();
    end // initial begin
 
    // Connect DUT to test bench
-   alu_rom alu_rom (.noe(noe),
-		    .flin(flin), .xin(xin),
+   alu_rom alu_rom (.nromoe(nromoe),
+		    .fl(fl), .x_in(x_in),
 		    .op(op), .a(a), .b(b),
 		    .ibus(ibus),
-		    .fvout(fvout), .nsetv(nsetv),
-		    .flout(flout), .nsetl(nsetl));
+		    .fvout_rom(fvout_rom), .nsetv_rom(nsetv_rom),
+		    .flout_rom(flout_rom), .nsetl_rom(nsetl_rom));
 
 
    // Verify our findings.
@@ -130,17 +131,17 @@ module alu_rom_tb();
    reg 		  correct_nbusen;
 
    // Check tri-stating
-   always @ (noe) begin
+   always @ (nromoe) begin
       #30 begin
    	 msg[7:0] = "";		// Use the msg as a flag.
 
-	 if (noe === 1) begin
+	 if (nromoe === 1) begin
 	   if (ibus !== 16'bZ) begin
-	      $sformat(msg, "noe=%b, but ibus=%b (should be Z)", noe, ibus);
+	      $sformat(msg, "nromoe=%b, but ibus=%b (should be Z)", nromoe, ibus);
 	   end
 	 end
 
-	 else if (noe !== 0) $sformat(msg, "testbench bug, noe=%b", noe);
+	 else if (nromoe !== 0) $sformat(msg, "testbench bug, nromoe=%b", nromoe);
 
    	 // Fail if we've logged an issue.
    	 if (msg[7:0]) begin
@@ -148,29 +149,29 @@ module alu_rom_tb();
    	    $error("assertion failure");
    	    #100 $finish;
    	 end
-   	 else $display("OK noe");
+   	 else $display("OK nromoe");
       end
-   end // always @ (noe)
+   end // always @ (nromoe)
 
 
    // Check results
    reg [31:0]          correct_y;
    reg 	               correct_l, correct_v;
-   wire signed [15:0]  as, bs, ibuss, flins;
+   wire signed [15:0]  as, bs, ibuss, fls;
    reg signed [31:0]   unbounded_ys;
 
    assign as = a;
    assign bs = b;
    assign ibuss = ibus;
-   assign flins = { 14'b0, flin};
+   assign fls = { 14'b0, fl};
 
    always @ (op, a, b) begin
       #350 begin
    	 msg[7:0] = "";		// Use the msg as a flag.
 
-	 if (noe === 0) begin
+	 if (nromoe === 0) begin
 	    casex (op)
-	      3'b000: correct_y = a + b + flin;	// ADD
+	      3'b000: correct_y = a + b + fl;	// ADD
 	      3'b001: correct_y = a & b;	// AND
 	      3'b010: correct_y = a | b;	// OR
 	      3'b011: correct_y = a ^ b;	// XOR
@@ -182,43 +183,43 @@ module alu_rom_tb();
 	    endcase // casex (op)
 
 	    if (ibus !== correct_y[15:0]) begin
-	       $sformat(msg, "noe=%b, a=%04x, b=%04x, op=%3b, but ibus=%04x (expecting %04x)",
-			noe, a, b, op, ibus, correct_y[15:0]);
+	       $sformat(msg, "nromoe=%b, a=%04x, b=%04x, op=%3b, but ibus=%04x (expecting %04x)",
+			nromoe, a, b, op, ibus, correct_y[15:0]);
 	    end
 
 	    // Only ADD should be setting L and V in this version of the
 	    // ALU ROMs. And ADD should ALWAYS set them.
 	    if (op === 3'b000) begin
-	       if (nsetl === 1)      $sformat(msg, "op=%03b but nsetl=%b (should be 0)", op, nsetl);
-	       else if (nsetv === 1) $sformat(msg, "op=%03b but nsetv=%b (should be 0)", op, nsetv);
-	       else if (nsetl !== 0) $sformat(msg, "testbench bug, nsetl=%b", nsetl);
-	       else if (nsetv !== 0) $sformat(msg, "testbench bug, nsetv=%b", nsetv);
+	       if (nsetl_rom === 1)      $sformat(msg, "op=%03b but nsetl_rom=%b (should be 0)", op, nsetl_rom);
+	       else if (nsetv_rom === 1) $sformat(msg, "op=%03b but nsetv_rom=%b (should be 0)", op, nsetv_rom);
+	       else if (nsetl_rom !== 0) $sformat(msg, "testbench bug, nsetl_rom=%b", nsetl_rom);
+	       else if (nsetv_rom !== 0) $sformat(msg, "testbench bug, nsetv_rom=%b", nsetv_rom);
 	       
-	       correct_l = a + b + flin > 65535 ? 1'b1 : 1'b0;
-	       if (flout != correct_l) begin
+	       correct_l = a + b + fl > 65535 ? 1'b1 : 1'b0;
+	       if (flout_rom != correct_l) begin
 		  $sformat(msg, "%0d + %0d -> %0d, L=%b (should be %b)",
-			   a, b, ibus, flout, correct_l);
+			   a, b, ibus, flout_rom, correct_l);
 	       end;
 
 	       // Overflow calculations using result comparison.
-	       unbounded_ys = as + bs + flins;
+	       unbounded_ys = as + bs + fls;
 	       correct_v = unbounded_ys != ibuss ? 1'b1 : 1'b0;
-	       if (correct_v !== fvout) begin
-		  $display("as=%0d, bs=%0d, flins=%0d, unbounded_ys=%0d, ibus=%0d. Equal? %d",
-			   as, bs, flins, unbounded_ys, ibuss, unbounded_ys == ibuss);
+	       if (correct_v !== fvout_rom) begin
+		  $display("as=%0d, bs=%0d, fls=%0d, unbounded_ys=%0d, ibus=%0d. Equal? %d",
+			   as, bs, fls, unbounded_ys, ibuss, unbounded_ys == ibuss);
 
 		  $sformat(msg, "%0d + %0d + %0d -> %0d (%04x + %04x + %0d-> %04x), V=%b (should be %b, correct unbounded result is %0d)",
-			   as, bs, flins, ibuss,
-			   a, b, flins, ibus,
-			   fvout, correct_v,
+			   as, bs, fls, ibuss,
+			   a, b, fls, ibus,
+			   fvout_rom, correct_v,
 			   unbounded_ys);
 	       end;
 	       
 	    end else begin
-	       if (nsetl === 0)      $sformat(msg, "op=%03b but nsetl=%b (should be 1)", op, nsetl);
-	       else if (nsetv === 0) $sformat(msg, "op=%03b but nsetv=%b (should be 1)", op, nsetv);
-	       else if (nsetl !== 1) $sformat(msg, "testbench bug, nsetl=%b", nsetl);
-	       else if (nsetv !== 1) $sformat(msg, "testbench bug, nsetv=%b", nsetv);
+	       if (nsetl_rom === 0)      $sformat(msg, "op=%03b but nsetl_rom=%b (should be 1)", op, nsetl_rom);
+	       else if (nsetv_rom === 0) $sformat(msg, "op=%03b but nsetv_rom=%b (should be 1)", op, nsetv_rom);
+	       else if (nsetl_rom !== 1) $sformat(msg, "testbench bug, nsetl_rom=%b", nsetl_rom);
+	       else if (nsetv_rom !== 1) $sformat(msg, "testbench bug, nsetv_rom=%b", nsetv_rom);
 	    end // else: !if(op === 3'b000)
 	 end
 
@@ -230,7 +231,7 @@ module alu_rom_tb();
    	 end
    	 else $display("OK %s", curop);
       end
-   end // always @ (noe)
+   end // always @ (nromoe)
 
 
 endmodule // buffer_245_tb
