ec96c33a713378c0515c065f93e913225041f39f
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-03 17:38:44 +0200

    Added new microaddress format.

--- verilog/filters/uaddr.c
+++ verilog/filters/uaddr.c
@@ -1,97 +1,73 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
-char *col[] = {
-	"",
-	"?red4?",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"?cyan4?",
-	"?green4?",
-	"?red4?",
-	"",
-};
+#include "../../microcode/build/microcode.h"
 
-char *instr[] = {
-	"TRAP",
-	"XXX",
-	"LOAD",
-	"STORE",
-	"IN",
-	"OUT",
-	"JMP",
-	"JSR",
-	"ADD",
-	"AND",
-	"OR",
-	"XOR",
-	"OP1",
-	"OP2",
-	"XXX",
-	"LI",
-};
-
-char * bin(int x)
-{
-	static char res [16];
-	int i;
-	for (i = 9; i >= 0; i--) {
-		res[9-i] = (x & (1<<i)) ? '1': '0';
+#define add(t,s) { \
+		if(t[0] && s[0]) {		\
+			strcat(t," ");		\
+		}				\
+		strcat(t,s);			\
 	}
-	res[10] = 0;
-	return res;
-}
 
 int main(int argc, char **argv)
 {
 	while(!feof(stdin))
 	{
-		char buf[1025], buf2[1025];
+		char buf[1025], buf2[1025], buf3[32], col[1024];
 		buf[0] = 0;
+		col[0] = 0;
 		if (fscanf(stdin, "%s", buf) != 1) continue;
 		if(buf[0]) {
 			unsigned int hx;
 			sscanf(buf, "%x", &hx);
 			buf2[0] = '\0';
 
-			/* RIVLOOOOISIAAAA          */
-			/* -----------------------  */
-			/* 123456789abcdeR = >>14 */
-			/*  123456789abdI = >>13   */
-			/*  123456789abV = >>12   */
-			/*  123456789abL = >>11   */
-			/*   1234567OOOO = >>7    */
-			/*       123456I = >>6    */
-			/*        12345S = >>5    */
-			/*         1234I = >>4    */
-			/*          xxxx = & 0xf  */
-			int rst = (hx >> 14) & 1;
-			int irq = (hx >> 13) & 1;
-			int v = (hx >> 12) & 1;
-			int l = (hx >> 11) & 1;
-			sprintf(buf2, "%sRST:%d IRQ:%d V:%d L:%d OP:%s I:%d SKIP:%d INC:%d uA:%d ",
-				(rst == 0) ? "?red4?" : (irq == 0) ? "?magenta4?" : col[(hx >> 7) & 0xf],
-				rst,
-				irq,
-				v,
-				l,
-				instr[(hx >> 7) & 0xf],
-				(hx >> 6) & 1,
-				(hx >> 5) & 1,
-				(hx >> 4) & 1,
-				hx & 0xf);
+			//  0 0001: upc
+			//  1 0002: upc
+			//  2 0004: upc
+			//  3 0008: upc
+			//  4 0010: idx
+			//  5 0020: idx
+			//  6 0040: !cond
+			//  7 0080: in_rsvd
+			//  8 0100: ir7
+			//  9 0200: ir8
+			// 10 0400: ir9
+			// 11 0800: ir10
+			// 12 1000: ir11
+			// 13 2000: ir12
+			// 14 4000: ir13
+			// 15 8000: ir14
+			// 16 10000: ir15
+			// 17 20000: irq
+			// 18 40000: reset
 
-			printf("%s\n", buf2);
+			if ((hx & 0x40000) == 0) {
+				strcpy(col, "?red4?");
+				add(buf2, "reset");
+			}
+			
+			if ((hx & 0x20000) == 0) {
+				strcpy(col, "?orange4?");
+				add(buf2, "irq");
+			}
+
+			snprintf(buf3, sizeof(buf3), "IR=%03x-", (hx & 0x1ff00) >> 5);
+			add(buf2, buf3);
+
+			if ((hx & 0x80) != 0) add(buf2, "(in_rsvd)");
+			if ((hx & 0x40) == 0) add(buf2, "cond=true");
+
+			if ((hx & 0x30) == 3) add(buf2, "idx-stack");
+			if ((hx & 0x30) == 2) add(buf2, "idx-adec");
+			if ((hx & 0x30) == 1) add(buf2, "idx-ainc");
+
+			if (!buf2[0]) add(buf2, "?red4?huh");
+			printf("%s%s\n", col, buf2);
 			fflush(stdout);
 		}
 	}
- 	return(0);
+	return(0);
 }
