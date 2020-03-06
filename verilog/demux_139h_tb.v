bc1d2e4453de10c887a96079640b2493bc6b6c3d
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-06 21:47:08 +0200

    Updating and adding Verilog testbenches.

--- verilog/demux_139h_tb.v
+++ verilog/demux_139h_tb.v
@@ -1,15 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// 74x139 BUFFER TESTBENCH
+// 74x139 (half) BUFFER TESTBENCH
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
 // REDESIGNED IN 2019
 // USES OK/FAIL OUTPUT
 //
-// buffer_139_tb.v -- 74x139 testbench
+// buffer_139h_tb.v -- 74x139 testbench (half the chip)
 //
-// Copyright © 2011–2019 Alexios Chouchoulas
+// Copyright © 2011–2020 Alexios Chouchoulas
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -30,54 +30,51 @@
 `include "demux.v"
 `timescale 1ns/10ps
 
-module demux_139_tb();
+module demux_139h_tb();
 
 // Declare inputs as regs and outputs as wires
-   reg [1:0]  a1, a2;
-   reg 	      ng1, ng2;
-   wire [3:0] y1, y2;
+   reg [1:0]  a;
+   reg 	      ng;
+   wire [3:0] y;
 
-   integer    i, j;
+   integer    i;
 
    // Initialize all variables
    initial begin        
-      $monitor("t: %7d | %b %b > %b | %b %b > %b", $time, ng1, a1, y1,  ng2, a2, y2);
-      $dumpfile ("vcd/demux_139_tb.vcd");
-      $dumpvars (0, demux_139_tb);
+      $monitor("t: %7d | %b %b > %b", $time, ng, a, y);
+      $dumpfile ("vcd/demux_139h_tb.vcd");
+      $dumpvars (0, demux_139h_tb);
 
-      ng1 = 1;
-      ng2 = 1;
-      a1 = 0;
-      a2 = 0;
+      ng = 1;
+      a = 0;
 
-      for (i = 0; i < 64; i = i + 1) begin
-	 #100 { a2, a1, ng2, ng1 } = i;
+      for (i = 0; i < 16; i = i + 1) begin
+	 #100 { a, ng } = i;
       end
 
       #200 $finish;      // Terminate simulation
    end
    
    // Connect DUT to test bench
-   demux_139 demux (.ng1(ng1), .a1(a1), .y1(y1), .ng2(ng2), .a2(a2), .y2(y2));
+   demux_139h demux (.ng(ng), .a(a), .y(y));
 
    // Verify our findings.
    reg [8191:0] msg;
-   reg [3:0] 	cy1, cy2;
+   reg [3:0] 	cy;
 
-   // Test unit 1
-   always @ (ng1, a1) begin
+   always @ (ng, a) begin
       #30 begin
    	 msg[7:0] = "";		// Use the msg as a flag.
 
-	 casex ({ng1, a1})
-	   {1'b1, 2'b??}: cy1 = 4'b1111;
-	   {1'b0, 2'b00}: cy1 = 4'b1110;
-	   {1'b0, 2'b01}: cy1 = 4'b1101;
-	   {1'b0, 2'b10}: cy1 = 4'b1011;
-	   {1'b0, 2'b11}: cy1 = 4'b0111;
+	 casex ({ng, a})
+	   {1'b1, 2'b??}: cy = 4'b1111;
+	   {1'b0, 2'b00}: cy = 4'b1110;
+	   {1'b0, 2'b01}: cy = 4'b1101;
+	   {1'b0, 2'b10}: cy = 4'b1011;
+	   {1'b0, 2'b11}: cy = 4'b0111;
 	 endcase // casex ({ng1, a1})
 
-	 if (y1 !== cy1) $sformat(msg, "ng1=%b, a1=%b, but y1=%b (should be %b)", ng1, a1, y1, cy1);
+	 if (y !== cy) $sformat(msg, "ng=%b, a=%b, but y=%b (should be %b)", ng, a, y, cy);
 	 
    	 // Fail if we've logged an issue.
    	 if (msg[7:0]) begin
@@ -89,29 +86,7 @@ module demux_139_tb();
       end
    end // always @ (ng1, a1)
 
-   // Test unit 2
-   always @ (ng2, a2) begin
-      #30 begin
-   	 msg[7:0] = "";		// Use the msg as a flag.
+endmodule // demux_139h_tb
 
-	 casex ({ng2, a2})
-	   {1'b1, 2'b??}: cy2 = 4'b1111;
-	   {1'b0, 2'b00}: cy2 = 4'b1110;
-	   {1'b0, 2'b01}: cy2 = 4'b1101;
-	   {1'b0, 2'b10}: cy2 = 4'b1011;
-	   {1'b0, 2'b11}: cy2 = 4'b0111;
-	 endcase // casex ({ng2, a2})
+// End of file.
 
-	 if (y2 !== cy2) $sformat(msg, "ng2=%b, a2=%b, but y2=%b (should be %b)", ng2, a2, y2, cy2);
-	 
-   	 // Fail if we've logged an issue.
-   	 if (msg[7:0]) begin
-   	    $display("346 FAIL assertion failed at t=%0d: %0s", $time, msg);
-   	    $error("assertion failure");
-   	    #100 $finish;
-   	 end
-   	 else $display("345 OK unit2");
-      end
-   end // always @ (ng2, a2)
-
-endmodule
