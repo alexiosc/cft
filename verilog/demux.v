2de2b99fb73b63c83cee331b35c86691571317ed
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-08 14:51:06 +0200

    Rewrote '138 and '238 demuxes to work around invisible (?) glitch in iverilog simulations.

--- verilog/demux.v
+++ verilog/demux.v
@@ -13,8 +13,6 @@
 
 module demux_138 (g1, ng2a, ng2b, a, y);
    parameter delay = 14;
-   parameter delay_g1 = 14;
-   parameter delay_g2 = 22;
 
    input        g1;		// Active high enable
    input        ng2a;		// Active low enable 1
@@ -23,48 +21,23 @@ module demux_138 (g1, ng2a, ng2b, a, y);
    
    output [7:0] y;		// Output.
    
-   wire 	g1, ng2a, ng2b;
-   wire 	g10, ng2a0, ng2b0;
-   wire [2:0] 	a;
-   
    reg [7:0] 	y0;
-   wire [7:0] 	y;
-   
-   wire 	q;
 
-   initial begin
-      // $display("BOM: 74x138");
-   end
+   assign #delay y = y0;
 
-   // The enable inputs have slightly different propagation
-   // delays. This could cause glitches. Simulate this.
-   assign #delay_g1 g10 = g1;
-   assign #delay_g2 ng2a0 = ng2a;
-   assign #delay_g2 ng2b0 = ng2b;
-   
-   // The 138 DOES NOT tri-state output. Instead, when disabled, all
-   // its active-low outputs are high.
-
-   // 2019-09-06: the simulated propagation delay caused glitches
-   // during transitions of A while gG1, NG2A and NG2B remained
-   // constant. The hardware '138 doesn't produce these, so this is a
-   // bug in this code. Removing delays for now.
-   assign y = (g1 && !ng2a && !ng2b) ? y0 : 8'b11111111;
-   
-   always @ (a) begin
-      case (a)
-	0: y0 = 8'b11111110;
-	1: y0 = 8'b11111101;
-	2: y0 = 8'b11111011;
-	3: y0 = 8'b11110111;
-	4: y0 = 8'b11101111;
-	5: y0 = 8'b11011111;
-	6: y0 = 8'b10111111;
-	7: y0 = 8'b01111111;
-	default: y0 = 8'b11111111; // Could happen when a=X.
+   always @ ({g1, ng2a, ng2b, a}) begin
+      case ({g1, ng2a, ng2b, a})
+	6'b100_000: y0 <= 8'b11111110;
+	6'b100_001: y0 <= 8'b11111101;
+	6'b100_010: y0 <= 8'b11111011;
+	6'b100_011: y0 <= 8'b11110111;
+	6'b100_100: y0 <= 8'b11101111;
+	6'b100_101: y0 <= 8'b11011111;
+	6'b100_110: y0 <= 8'b10111111;
+	6'b100_111: y0 <= 8'b01111111;
+	default:    y0 <= 8'b11111111;
       endcase // case (a)
-   end // always @ (a)
-
+   end // always @ ({g1, ng2a, ng2b, a})
 endmodule // demux_138
 
 
@@ -130,9 +103,7 @@ endmodule // demux_139h
 
 // Just like a '138, but with active-high outputs
 module demux_238 (g1, ng2a, ng2b, a, y);
-   parameter delay = 18;
-   parameter delay_g1 = 21;
-   parameter delay_g2 = 20;
+   parameter delay = 14;
 
    input        g1;		// Active high enable
    input        ng2a;		// Active low enable 1
@@ -141,49 +112,24 @@ module demux_238 (g1, ng2a, ng2b, a, y);
    
    output [7:0] y;		// Output.
    
-   wire 	g1, ng2a, ng2b;
-   wire 	g10, ng2a0, ng2b0;
-   wire [2:0] 	a;
-   
    reg [7:0] 	y0;
-   wire [7:0] 	y;
-   
-   wire 	q;
 
-   initial begin
-      // $display("BOM: 74x238");
-   end
+   assign #delay y = y0;
 
-   // The enable inputs have slightly different propagation
-   // delays. This could cause glitches. Simulate this.
-   assign #delay_g1 g10 = g1;
-   assign #delay_g2 ng2a0 = ng2a;
-   assign #delay_g2 ng2b0 = ng2b;
-   
-   // The 138 DOES NOT tri-state output. Instead, when disabled, all
-   // its active-low outputs are high.
-
-   // 2019-09-06: the simulated propagation delay caused glitches
-   // during transitions of A while G1, nG2A and nG2B remained
-   // constant. The hardware '138 doesn't produce these, so this is a
-   // bug in this code. Removing delays for now.
-   assign y = (g1 && !ng2a && !ng2b) ? y0 : 8'b00000000;
-   
-   always @ (a) begin
-      case (a)
-	0: y0 = 8'b00000001;
-	1: y0 = 8'b00000010;
-	2: y0 = 8'b00000100;
-	3: y0 = 8'b00001000;
-	4: y0 = 8'b00010000;
-	5: y0 = 8'b00100000;
-	6: y0 = 8'b01000000;
-	7: y0 = 8'b10000000;
-	default: y0 = 8'b00000000; // Could happen when a=X.
+   always @ ({g1, ng2a, ng2b, a}) begin
+      case ({g1, ng2a, ng2b, a})
+	6'b100_000: y0 <= 8'b00000001;
+	6'b100_001: y0 <= 8'b00000010;
+	6'b100_010: y0 <= 8'b00000100;
+	6'b100_011: y0 <= 8'b00001000;
+	6'b100_100: y0 <= 8'b00010000;
+	6'b100_101: y0 <= 8'b00100000;
+	6'b100_110: y0 <= 8'b01000000;
+	6'b100_111: y0 <= 8'b10000000;
+	default:    y0 <= 8'b00000000;
       endcase // case (a)
-   end // always @ (a)
-
-endmodule // demux_138
+   end // always @ ({g1, ng2a, ng2b, a})
+endmodule // demux_238
 
 
 `endif //  `ifndef demux_v
