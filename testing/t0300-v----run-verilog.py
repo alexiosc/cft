29b735e3d6782c4f843ff90cba6c870804cab92f
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-18 13:50:49 +0200

    Added first programmatic verilog tests.

---
+++ testing/t0300-v----run-verilog.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+import array
+import pytest
+import inspect
+import subprocess
+
+from testing import * 
+
+
+@
+def test_verilog_experiment(capsys, tmpdir):
+
+    source = """
+    &0:     OUT R &11e     ; SUCCESS
+            OUT R &11d     ; Halt immediately.
+    """
+
+    halted = True
+    result = [ x[0] for x in run_on_verilog_emu(capsys, tmpdir, source) ]
+    assert result == [ OK, OK, HALTED ]
+
+    
+
+
+if __name__ == "__main__":
+    print("Run this with pytest-3!")
+
+# End of file.
