83910832c4c69b3417384099367e05e88297ce7d
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-18 22:19:16 +0200

    Re-added LI test

--- testing/t0300-v----run-verilog.py
+++ testing/t0300-v----run-verilog.py
@@ -24,6 +24,22 @@ def test_verilog_experiment(capsys, tmpdir):
     assert expected == list(result)
 
     
+@pytest.mark.verilog
+def test_LI(capsys, tmpdir):
+
+    source, expected = "&0:\n", ExpectedData([ SUCCESS ])
+
+    for x in range(1024):
+        source += "LI &{:>x}\n".format(x)
+        source += "OUT R &113\n"
+        expected.append([ 340, "PRINTU", str(x) ])
+
+    source += "OUT R &11d\n"
+    expected += [ HALTED ]
+
+    result = run_on_verilog_emu(capsys, tmpdir, source)
+    result = list(expected.prepare(result))
+    assert result == expected
 
 
 if __name__ == "__main__":
