59c598b49bbe29c7464a6d0a494663f774e2e904
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-26 22:30:05 +0200

    Working on tests.

--- testing/t1120-vedh-STORE.py
+++ testing/t1120-vedh-STORE.py
@@ -202,34 +202,119 @@ def test_STORE_I_R(capsys, tmpdir):
     assert result == expected
 
 
-# # @pytest.mark.verilog
-# # def test_LOAD_I_R_autoinc(capsys, tmpdir):
+# @pytest.mark.verilog
+# def test_STORE_I_R_autoinc(capsys, tmpdir):
 
-# #     source = "&340:\n"
-# #     source += "        .word data"
-# #     expected = ExpectedData([ SUCCESS ])
+#     source = ".equ autoinc &340\n"
 
-# #     MAX = 5                   # 1 to 255
+#     expected = ExpectedData([ SUCCESS ])
 
-# #     source = "&0:\n"
-# #     for x in range(MAX):
-# #         source += "        LOAD  I R &340\n"
-# #         source += "        OUT   R &113\n"
-# #         expected.append([ 340, "PRINTU", str(x) ])
+#     MAX = 255                   # 1 to 255
 
-# #     source += "        OUT R &11d\n"
-# #     expected += [ HALTED ]
+#     source += "&0:\n"
+#     source += asm_memory_banks(mbp=0x80, mbd=0, mbz=0)
+#     source += "\t\tLOAD      @+3\n"
+#     source += "\t\tSTORE R   autoinc\n"
+#     source += "\t\tJMP       @+2\n"
+#     source += "\t\t.word     data\n"
 
-# #     # The data table.
-# #     source += "&1389:\n"
-# #     source += "data:\n"
+#     for x in range(MAX):
+#         source += "\t\tLOAD I R  autoinc\n"
+#         source += "\t\tOUT R     &113\n"
+#         expected.append([ 340, "PRINTU", str(x) ])
 
-# #     for x in range(MAX):
-# #         source += "        .word {}\n".format(x)
+#     source += "\t\tOUT R &11d\n"
+#     expected += [ HALTED ]
+
+#     # The data table.
+#     source += "&1389:\n"
+#     source += "data:\n"
+
+#     for x in range(MAX):
+#         source += "\t\t.word {}\n".format(x)
     
-# #     result = run_on_verilog_emu(capsys, tmpdir, source)
-# #     result = list(expected.prepare(result))
-# #     assert result == expected
+#     result = run_on_verilog_emu(capsys, tmpdir, source)
+#     result = list(expected.prepare(result))
+#     assert result == expected
+
+
+# @pytest.mark.verilog
+# def test_STORE_I_R_autodec(capsys, tmpdir):
+
+#     source = ".equ autodec &380\n"
+
+#     expected = ExpectedData([ SUCCESS ])
+
+#     MAX = 255                   # 1 to 255
+
+#     source += "&0:\n"
+#     source += asm_memory_banks(mbp=0x80, mbd=0, mbz=0)
+#     source += "\t\tLOAD      @+3\n"
+#     source += "\t\tSTORE R   autodec\n"
+#     source += "\t\tJMP       @+2\n"
+#     source += "\t\t.word     @data-1\n"
+
+#     for x in range(MAX):
+#         source += "\t\tLOAD I R  autodec\n"
+#         source += "\t\tdfp.PRINTU\n"
+#         expected.append([ 340, "PRINTU", str(MAX - 1 - x) ])
+
+#     source += "\t\tHALT\n"
+#     expected += [ HALTED ]
+
+#     # The data table.
+#     source += "&1389:\n"
+
+#     for x in range(MAX):
+#         source += "\t\t.word {}\n".format(x)
+#     source += "data:\n"
+    
+#     result = run_on_verilog_emu(capsys, tmpdir, source)
+#     result = list(expected.prepare(result))
+#     assert result == expected
+
+
+# @pytest.mark.verilog
+# def test_STORE_I_R_stack(capsys, tmpdir):
+#     """Test stack pointer autoindexing. For LOAD, this behaves just like
+#     autodecrement except the register is decremented BEFORE use."""
+
+#     source = ".equ stack &3c0\n"
+
+#     expected = ExpectedData([ SUCCESS ])
+
+#     MAX = 255                   # 1 to 255
+
+#     source += "&0:\n"
+#     source += asm_memory_banks(mbp=0x80, mbd=0, mbz=0)
+#     source += "\t\tLOAD      @+3\n"
+#     source += "\t\tSTORE R   stack\n"
+#     source += "\t\tJMP       @+2\n"
+#     source += "\t\t.word     data\n"
+
+#     for x in range(MAX):
+#         source += "\t\tLOAD I R  stack\n"
+#         source += "\t\tOUT R     &113\n"
+#         expected.append([ 340, "PRINTU", str(MAX - 1 - x) ])
+
+#     source += "\t\tLOAD R    stack\n"
+#     source += "\t\tOUT R     &114\n"
+#     expected += [ [340, "PRINTH", "1389" ] ]
+
+#     source += "\t\tOUT R &11d\n"
+#     expected += [ HALTED ]
+
+#     # The data table.
+#     source += "&1389:\n"
+
+#     for x in range(MAX):
+#         source += "\t\t.word {}\n".format(x)
+#     source += "data:\n"
+    
+#     result = run_on_verilog_emu(capsys, tmpdir, source)
+#     result = list(expected.prepare(result))
+#     assert result == expected
+
 
 
 if __name__ == "__main__":
