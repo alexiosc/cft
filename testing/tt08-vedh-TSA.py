44ec26dccf37d3e56ef09cb56756b6b6a9b5973f
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-03-31 11:02:41 +0300

    Added simple TAS and TSA instruction tests.

---
+++ testing/tt08-vedh-TSA.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+import array
+import pprint
+import pytest
+import inspect
+import subprocess
+
+from testing import * 
+
+
+@pytest.mark.verilog
+@pytest.mark.emulator
+@pytest.mark.hardware
+@pytest.mark.LOAD
+@pytest.mark.PHA
+@pytest.mark.TAS
+@pytest.mark.TSA
+def test_TSA(capsys, tmpdir):
+    source = """
+    .include "mbu.asm"
+    .include "dfp2.asm"
+
+    &0:
+            LI &80        ; Configure essential MBRs and enable.
+            SMB mbu.MB0
+            LI &00
+            SMB mbu.MBS
+
+            LI 0          ; Clear the stack pointer
+            TAS
+
+            LI 42
+            TSA
+            dfp.PRINTD
+            
+            LI 666
+            PHA
+            PHA
+            PHA
+            PHA
+            PHA
+
+            TSA
+            dfp.PRINTD
+
+            PHA
+            PHA
+            PHA
+            PHA
+            PHA
+
+            TSA
+            dfp.PRINTD
+
+            HALT
+    """
+
+    expected = ExpectedData([
+        SUCCESS,
+        [ 340, "PRINTD", "0" ],
+        [ 340, "PRINTD", "5" ],
+        [ 340, "PRINTD", "10" ],
+        HALTED
+    ])
+
+    result = run_on_verilog_emu(capsys, tmpdir, source)
+    result = list(expected.prepare(result))
+    assert list(result) == expected
+
+
+if __name__ == "__main__":
+    print("Run this with pytest-3!")
+
+# End of file.
