2f151351becec36dab01d0f4ab22cd0f83d677db
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-04-01 21:52:56 +0300

    New tests added.

---
+++ testing/t1108-vedh-TAD.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+import array
+import pprint
+import pytest
+import inspect
+import subprocess
+
+from testing import * 
+
+
+@pytest.mark.verilog
+@pytest.mark.emulator
+@pytest.mark.hardware
+@pytest.mark.LOAD
+@pytest.mark.STORE
+@pytest.mark.DSZ
+@pytest.mark.TAD
+@pytest.mark.TDA
+def test_TAD(capsys, tmpdir):
+
+    reps = 1024
+    
+    source = """
+    .include "mbu.asm"
+    .include "dfp2.asm"
+
+    &0:
+            LI &80        ; Configure essential MBRs and enable.
+            SMB mbu.MB0
+            LI &00
+            SMB mbu.MBZ
+
+            LOAD reps
+            STORE R &200
+
+    loop:   TAD
+            LI 42
+            TDA
+            dfp.PRINTD
+            DSZ R &200
+            JMP loop
+
+            HALT
+
+    reps:   .data {}
+    """.format(reps)
+
+    expected = ExpectedData([ SUCCESS ])
+    expected += [ [ 340, "PRINTD", str(x)] for x in range(reps, -1, -1) ]
+    expected += [ HALTED ]
+
+    result = run_on_verilog_emu(capsys, tmpdir, source)
+    result = list(expected.prepare(result))
+    assert list(result) == expected
+
+
+if __name__ == "__main__":
+    print("Run this with pytest-3!")
+
+# End of file.
