6b12561631f418464725aaceb3c786271abd3dea
Alexios Chouchoulas <alexios@bedroomlan.org>
2020-11-10 01:57:29 +0200

    Clear some unused definitions

--- peripherals/dfp2/lib/iface.h
+++ peripherals/dfp2/lib/iface.h
@@ -37,7 +37,7 @@ typedef struct {
         void            (*io_w)(uint32_t ab, uint16_t db);
 
         // Simulate Front Panel Bus
-        uint8_t         (*fpd_r)(uint8_t fpaddr);
+        uint8_t         fpd[256];
 
         // The Front Panel Lights (set the DFP, displayed by the emulator)
         uint8_t         lights[20];
@@ -45,34 +45,12 @@ typedef struct {
         // The Front Panel Switches (set the emulator, used by the DFP)
         uint8_t         switches[8];
 
-	// uint16_t        (*unit_mem)(int r, int w);
-	// uint16_t        (*unit_io)(int r, int w);
-
 	// int             request_testmode;
 	// int             request_unpause;
 	// int             request_reset;
 	// int             request_halt;
 	// int             request_run;
 	// int             changed_sr;
-
-	// uint16_t  * ir;
-	// uint16_t  * pc;
-	// uint16_t  * ac;
-
-	// uint8_t   * l;		// A single uint8_t for the L register
-	// uint8_t   * i;		// A single uint8_t for the I register
-	
-	// uint8_t   * v;		// V flag
-	// uint8_t   * n;		// N flag
-	// uint8_t   * z;		// Z flag
-
-	// uint16_t  * ab;
-	// uint16_t  * db;
-	// uint16_t  * swright;
-	// uint16_t  * swleft;
-	// uint16_t  * sr;
-
-	// uint32_t  * uvec;	// Microcode control vector
 
 	pthread_mutex_t lock;
 	pthread_mutex_t rx_lock;
