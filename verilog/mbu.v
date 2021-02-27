b0197f4e3d7d6e27f30baed110f7cf1ba3330f7c
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-02-27 22:06:06 +0200

    Work on MBU

--- verilog/mbu.v
+++ verilog/mbu.v
@@ -22,7 +22,7 @@
 `ifndef mbu_v
 `define mbu_v
 
-`include "rom.v"
+`include "ram.v"
 `include "mux.v"
 `include "demux.v"
 `include "buffer.v"
@@ -81,11 +81,12 @@ module mbu_control_gal(ndis,	// MBU Disable
 		       nidxen,
 		       nwmbp,
 		       nwmbn,
-		       raddr1_0,	// RADDR[1:0]
-		       ir2_0,	// IR[2:0]
+		       raddr1_0, // RADDR[1:0]
+		       ir2_0,	 // IR[2:0]
 		       a,
 		       noe,
-		       nwe
+		       nwe,
+		       nibusen
 		       );
    
    parameter delay = 15;
@@ -103,6 +104,7 @@ module mbu_control_gal(ndis,	// MBU Disable
    output [2:0] a;
    output 	noe;
    output 	nwe;
+   output 	nibusen;
 
    // Taken directly from running:
    // 
@@ -111,7 +113,7 @@ module mbu_control_gal(ndis,	// MBU Disable
 
    // Map galasm name to verilog names
    wire 	dis, rmbp, rmbn, war, wmbn, wmbp, iden, addr0, addr1, ir0, ir1, ir2;
-   wire 	a0, a1, a2, oe, we;
+   wire 	a0, a1, a2, oe, we, ibusen;
 
    assign dis = ndis;
    assign rmbp = nrmbp;
@@ -129,35 +131,42 @@ module mbu_control_gal(ndis,	// MBU Disable
    assign a = {a2, a1, a0};
    assign noe = oe;
    assign nwe = we;
+   assign nibusen = ibusen;
 
    // REVISION: MBUCTL00
 
-
-   assign #delay a2 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir2 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir2 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir2 == 1'b1);
-
-   assign #delay a1 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir1 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (addr1 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir1 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir1 == 1'b1);
-
-   assign #delay a0 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir0 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (addr0 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir0 == 1'b1) || 
-		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir0 == 1'b1);
 
    assign #delay oe = (dis == 1'b0) || 
 		      (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) || 
 		      (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b0) && (wmbn == 1'b1);
 
    assign #delay we = (dis == 1'b0) || 
+		      (rmbp == 1'b1) && (rmbn == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) || 
 		      (rmbp == 1'b0) || 
 		      (rmbn == 1'b0) || 
 		      (war == 1'b0) || 
-		      (wmbp == 1'b1) && (wmbn == 1'b1) || 
 		      (wmbp == 1'b0) && (wmbn == 1'b0);
 
+   assign #delay ibusen = (dis == 1'b0) || 
+			  (rmbp == 1'b1) && (rmbn == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) || 
+			  (rmbp == 1'b0) && (rmbn == 1'b0) || 
+			  (war == 1'b0) || 
+			  (wmbp == 1'b0) || 
+			  (wmbn == 1'b0);
+
+   assign #delay a0 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir0 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (addr0 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir0 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir0 == 1'b1);
+
+   assign #delay a1 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir1 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (addr1 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir1 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir1 == 1'b1);
+
+   assign #delay a2 = (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b0) && (ir2 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b1) && (war == 1'b0) && (iden == 1'b0) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir2 == 1'b1) || 
+		      (dis == 1'b1) && (rmbp == 1'b1) && (rmbn == 1'b0) && (war == 1'b1) && (wmbp == 1'b1) && (wmbn == 1'b1) && (ir2 == 1'b1);
 endmodule // mbu_control_gal
 
 
@@ -165,73 +174,106 @@ endmodule // mbu_control_gal
 
 // The Memory Bank Register File
 
-module mbu (nreset,
-	    clk1, clk2, clk3, clk4, t34,
+module mbu (nrsthold,
+	    clk2, clk4, t34,
 	    raddr,
 	    waddr,
+	    ibus,		// Only bits 0–7 of the IBUS are used.
 	    nir_idx,		// During nwar, CU wants ir[2:0] to select MBR
 	    ir,			// Only bits 0–2 are used.
-	    ibus,		// Only bits 0–7 of the IBUS are used.
 	    aext,		//
-	    nr, nw,
-	    ab,	db, nsysdev,	// Only bits 0–7 of the AB and DB are used.
-	    nwar,		// Convenience output to the AR
-	    nfpram_rom		// RAM/ROM switch from front panel
+	    nfpram_rom,		// RAM/ROM switch from front panel
+	    nwar		// Convenience output to the AR
 	    );
    
-   input        nreset;
-   input 	clk1, clk2, clk3, clk4, t34;
+   input        nrsthold;
+   input 	clk2, clk4, t34;
    input [4:0] 	waddr, raddr;
-   input 	nir_idx;
    input [2:0] 	ir;
-   input 	nsysdev;
-   input 	nr;
-   input 	nw;
+   input 	nir_idx;
    inout 	nfpram_rom;
-   
    inout [7:0] 	ibus;
-   inout [7:0] 	ab;
-   inout [15:0] db;		// Real hardware only uses 7:0.
    
-   output [7:0] aext;
+   inout [7:0] 	aext;
    output 	nwar;
-
-   wire 	nreset;
-   wire [4:0] 	waddr;
-   wire [4:0] 	raddr;
-   wire [7:0] 	ibus;
 
    tri0 [7:0] 	aext;
    tri1 	nfpram_rom;
 
    ///////////////////////////////////////////////////////////////////////////////
    // 
-   // DECODING		
+   // DECODING
+   //
+   ///////////////////////////////////////////////////////////////////////////////
+   
+   wire 	nrmbn, nrmbp, nrctx;
+   wire 	nwmbn, nwmbp, nwctx;
+
+   mbu_decoder_gal decoder_gal(
+			       .raddr(raddr),	// CFT Read Unit
+			       .waddr(waddr),	// CFT WRite Unit
+			       .clk4(clk4),	// Write strobe
+			       .nrmbn(nrmbn),	// Put MBn on IBus
+			       .nrmbp(nrmbp),	// Put MBP (MB0) on IBus
+			       .nrctx(nrctx),	// Put CTX on IBus
+			       .nwmbn(nwmbn),	// Write IBus to MBn
+			       .nwmbp(nwmbp),   // Write IBus to MBp
+			       .nwctx(nwctx),   // Write IBus to CTX 
+			       .nwar(nwar)      // One of the four write_ar_xx strobes
+			       );   
+
+   ///////////////////////////////////////////////////////////////////////////////
+   // 
+   // CONTROL
    //
    ///////////////////////////////////////////////////////////////////////////////
 
-   wire [7:0] 	dec_ab, dec_raddr, dec_waddr1, dec_waddr2;
-   wire 	niombr;
-   wire 	nrmbp;
-   wire 	nwmbp;
-   wire 	nwar;
-
-   // U10: This decodes I/O addresses &008–&00F.
-   demux_138 demux_ab (.a(ab[6:4]), .g1(ab[3]), .ng2a(ab[7]), .ng2b(nsysdev), .y(dec_ab));
-   assign niombr = dec_ab[0];
+   wire 	ndis;
+   wire 	nir_idxr;
+   wire [2:0] 	a;
+   wire 	noe;
+   wire 	nwe;
+   wire 	nibusen;
    
-   // U12: We decode RADDR 01101, read_mbp+flags.
-   // demux_138 demux_raddr (.a(raddr[2:0]), .g1(raddr[3]), .ng2a(raddr[4]), .ng2b(t34), .y(dec_raddr));
-   demux_138 demux_raddr (.a(raddr[2:0]), .g1(raddr[3]), .ng2a(raddr[4]), .ng2b(1'b0), .y(dec_raddr));
-   assign nrmbp = dec_raddr[5];
+   mbu_control_gal control_gal(
+			       .ndis(ndis),	// MBU Disable
+			       .nrmbp(nrmbp),
+			       .nrmbn(nrmbn),
+			       .nwar(nwar),
+			       .nidxen(nir_idxr),
+			       .nwmbp(nwmbp),
+			       .nwmbn(nwmbn),
+			       .raddr1_0(raddr[1:0] ),	// RADDR[1:0]
+			       .ir2_0(ir[2:0]),
+			       .a(a),
+			       .noe(noe),
+			       .nwe(nwe),
+			       .nibusen(nibusen)
+			       );
 
-   // U16: We decode WADDRs 01100, 01101, and 01110. Symmetric to the above '138.
-   demux_138 demux_waddr1 (.a(waddr[3:1]), .g1(1'b1), .ng2a(waddr[4]), .ng2b(clk4), .y(dec_waddr1));
-   assign nwmbp = dec_waddr1[6];
 
-   // U17: decode WADDR to get nwar.
-   demux_138 demux_waddr2 (.a(waddr[4:2]), .g1(1'b1), .ng2a(1'b0), .ng2b(1'b0), .y(dec_waddr2));
-   assign nwar = dec_waddr2[1];
+   ///////////////////////////////////////////////////////////////////////////////
+   // 
+   // THE CONTEXT REGISTER
+   //
+   ///////////////////////////////////////////////////////////////////////////////
+
+   wire [7:0] 	ctx;
+   
+   flipflop_574 ff_ctx (.d(ibus[7:0]), .q(ctx), .clk(nwctx), .noe(1'b0));
+   buffer_541 buf_ctx (.noe1(nrctx), .noe2(1'b0), .a(ctx), .y(ibus[7:0]));
+
+
+   ///////////////////////////////////////////////////////////////////////////////
+   // 
+   // THE REGISTER FILE
+   //
+   ///////////////////////////////////////////////////////////////////////////////
+
+   sram #(11, 15) regfile_mbu (.a({ctx, a}), .d(aext), .nce(1'b1), .nwe(nwe), .noe(noe));
+   buffer_541 buf_mbu_in (.noe1(nwe), .noe2(1'b0), .a(ibus[7:0]), .y(aext));
+   buffer_541 buf_mbu_out (.noe1(nibusen), .noe2(t34), .a(aext), .y(ibus[7:0]));
+
 
    ///////////////////////////////////////////////////////////////////////////////
    // 
@@ -247,167 +289,11 @@ module mbu (nreset,
    // U18: the MBU enable FF. We output complementary active-low pair (nEN,
    // nDIS). nEN is also connected to a LED, but there's a solder jumper to
    // disconnect it if the current draw becomes too much.
-   wire 	nen, ndis, niowmbr;
-   flipflop_74h ff_init(.d(1'b1), .clk(1'b1), .nset(nreset), .nrst(niowmbr),
-			.q(nen), .nq(ndis));
+   flipflop_74h ff_init(.d(1'b1), .clk(1'b1), .nset(nwmbn), .nrst(nrsthold), 
+			.q(ndis));
 
-   ///////////////////////////////////////////////////////////////////////////////
-   // 
-   // THE REGISTER FILE
-   //
-   ///////////////////////////////////////////////////////////////////////////////
-
-   wire [1:0] 	ra, wa;
-   wire [7:0] 	rd;
-   wire 	nren0, nren1;
-   wire 	nwen0, nwen1;
-
-   // 8 8-bit registers formed out of 4 4×4-bit '670 register files. These are
-   // sluggish chips compared to the SRAM of the previous design, but simpler
-   // to use.
-   regfile_670 rf_0lo (.d(ibus[3:0]), .nre(nren0), .nwe(nwen0), .ra(ra), .wa(wa), .q(rd[3:0]));
-   regfile_670 rf_0hi (.d(ibus[7:4]), .nre(nren0), .nwe(nwen0), .ra(ra), .wa(wa), .q(rd[7:4]));
-
-   regfile_670 rf_1lo (.d(ibus[3:0]), .nre(nren1), .nwe(nwen1), .ra(ra), .wa(wa), .q(rd[3:0]));
-   regfile_670 rf_1hi (.d(ibus[7:4]), .nre(nren1), .nwe(nwen1), .ra(ra), .wa(wa), .q(rd[7:4]));
-
-   // Connect the RD bus to AEXT when the MBU is enabled. We use a multiplexer
-   // (and no pull-down resistors) for bit 7 because it changes depending on
-   // the value of nfpram_rom.
-   wire [7:0] 	buf_aext_y;
-   buffer_541 buf_aext (.noe1(nen), .noe2(1'b0), .a({1'b0, rd[6:0]}), .y(buf_aext_y));
-   mux_2g157 mux_aext7 (.sel(nen), .a(rd[7]), .b(nfpram_rom), .ng(1'b0), .y(aext[7]));
-   assign aext[6:0] = buf_aext_y;
-
-   ///////////////////////////////////////////////////////////////////////////////
-   //
-   // WRITE ADDRESSING
-   //
-   ///////////////////////////////////////////////////////////////////////////////
-
-   // The MBU registers are written to in response to two actions:
-   //
-   // (a) When the Control Unit asserts write_mbp or write_mbp+flags (decoded
-   //     as nwmbp here).
-   // (b) Programmatically, using the When the Control Unit asserts write_mbp
-   //     or write_mbp+flags (decoded as niowmbr).
-   //
-   // In case (a), the address is always 000 (MBP).
-   // In case (b), the address is AB[2:0].
-   // ____   ______                ____   ____
-   // WBMP   IOWMBR    WA0   WA1   WEN0   WEN1   What
-   // ------------------------------------------------------------------------
-   //   1      1        X     X     1       1    No write
-   //   0      1        0     0     0       1    Write to MBP
-   //   1      0       AB0   AB1   AB2    !AB2   Write to MB[AB[2:0]]
-   //
-   // To implement this, we use one stages of three multiplexers plus two
-   // 1g0832 AND/OR ICs.
-
-   mux_2g157 wmux0 (.a(ab[0]), .b(1'b0), .sel(niombr), .ng(1'b0), .y(wa[0]));
-   mux_2g157 wmux1 (.a(ab[1]), .b(1'b0), .sel(niombr), .ng(1'b0), .y(wa[1]));
-
-   // The third mux selects the correct bank of 670s for writing.
-   wire 	nw0, nw1;
-   mux_2g157 wmux2 (.a(ab[2]), .b(1'b0), .sel(niombr), .ng(1'b0), .y(nw0), .ny(nw1));
-
-   // And then generate write enables when (niombr and nw) or nmbp are asserted:
-   assign #4 niowmbr = niombr | nw;
-
-   // This uses two 74LVG1G0832 units:
-   assign #4 nwen0 = (niowmbr & nwmbp) | nw0;
-   assign #4 nwen1 = (niowmbr & nwmbp) | nw1;
-
-   // Timing note: the enables are delayed a little bit compared to the WA
-   // address multiplexing, but not enough for maintaining the '670s languid
-   // setup time (≥10ns). Luckily, IO addressing (and niombr) is decoded ~
-   // 62.5ns before the nw strobe (and thus ~64.5ns before niowmbr), so in
-   // practice this is no concern.
-
-   ///////////////////////////////////////////////////////////////////////////////
-   //
-   // READ ADDRESSING
-   //
-   ///////////////////////////////////////////////////////////////////////////////
-
-   // There are four ways to read MBU registers. The truth table is complex
-   // because it also implements auto-indexed addressing modes:
-   // ____  ___  ______  
-   // RMBP  WAR  IORMBR  WADDR  NIR_IDX  ADDR       What
-   // -------------------------------------------------------------------------
-   //  X     X    X          X    X         X      MBU disabled. AEXT=&00 or &80.
-   //  0     X    X          X    X       000      Reading MBP. AEXT drives IBus.
-   //  1     0    X      ...00    X       000      Indexing with MBP. Address: waddr[0:1]
-   //  1     0    X      ...01    X       001      Indexing with MBD.
-   //  1     0    X      ...10    X       010      Indexing with MBS.
-   //  1     0    X      ...11    0       011      Indexing with MBS.
-   //  1     0    X      ...11    1       ir[2:0]  Autoindexing, address is IR[2:0].
-   //  1     1    0          X    X       ab[1:0]  IN from MBR. AEXT drives DB.
-   // -------------------------------------------------------------------------
-
-   wire 	nrg, cur0, cur1, cur2;
-   assign #4 nrg = !nrmbp;
-
-   wire 	nir_idxreg;
-   // flipflop_74h ff_idx(.d(1'b0), .clk(t34), .nset(nir_idx), .nrst(nreset),
-   // 		       .nq(nir_idxreg));
-   flipflop_74h ff_idx(.d(1'b0), .clk(clk2), .nset(nir_idx), .nrst(nreset),
-		       .nq(nir_idxreg));
-
-   // FOR TESTING ONLY: when the MBU isn't being read from (RG# is high) and
-   // the AB is tri-stated, X values appear in Verilog waveforms because
-   // not(Z)=X.  On real hardware, the AB has Bus Hold circuitry so its values
-   // are never defined. The MBU would be driving AEXT with a bogus register,
-   // which isn't used anyway. On the simulator, we don't use Bus Hold on AB so
-   // that these problems are more obvious. So, to fix the X issue here, we
-   // treat AB[2:0] specially. To help locate this, we pull up the values to
-   // emit register 7.
-   wire [2:0] 	ab2_0;
-   assign ab2_0[0] = ab[0] === 1'bz ? 1 : ab[0];
-   assign ab2_0[1] = ab[1] === 1'bz ? 1 : ab[1];
-   assign ab2_0[2] = ab[2] === 1'bz ? 1 : ab[2];
-
-   // First stage mux, choose between IR and AB as source for AR indexing.
-   mux_2g157 rmux1_0 (.a(ir[0]), .b(waddr[0]), .sel(nir_idxreg), .ng(1'b0), .y(cur0));
-   mux_2g157 rmux1_1 (.a(ir[1]), .b(waddr[1]), .sel(nir_idxreg), .ng(1'b0), .y(cur1));
-   mux_2g157 rmux1_2 (.a(ir[2]), .b(1'b0),     .sel(nir_idxreg), .ng(1'b0), .y(cur2));
-
-   // Second stage mux, choose between the previous stage and AB[2:0].
-   mux_2g157 rmux2_0 (.a(cur0), .b(ab2_0[0]), .sel(nwar), .ng(nrg), .y(ra[0]));
-   mux_2g157 rmux2_1 (.a(cur1), .b(ab2_0[1]), .sel(nwar), .ng(nrg), .y(ra[1]));
-   mux_2g157 rmux2_2 (.a(cur2), .b(ab2_0[2]), .sel(nwar), .ng(nrg), .y(nren0), .ny(nren1));
-
-   ///////////////////////////////////////////////////////////////////////////////
-   //
-   // OUTPUT BUS INTERFACE
-   //
-   ///////////////////////////////////////////////////////////////////////////////
-
-   // There are three outputs:
-   //
-   // (a) AEXT (dedicated, always active, the MBU is the only driver)
-   // (b) IBUS[7:0] (we only drive bits 0–7 on Control Unit)
-   // (c) DB[7:0] (when an IN instruction is issued)
-
-   buffer_541 buf_ibus (.noe1(nrmbp), .noe2(1'b0), .a(aext), .y(ibus[7:0]));
-   buffer_541 buf_db   (.noe1(niombr), .noe2(nr), .a(aext), .y(db[7:0]));
-
-   // This is here ONLY for debugging, otherwise the testbench throws
-   // a spurious bus error: we need to drive all 16 bits of the IBUS.
-   //
-   // Note (2020-04-19): removed this, as read_bmp has been removed in
-   // favour of read_bmp+flags which always drives all 16 bits of the
-   // IBus.
-
-   // tri0 [7:0] 	_db_pullup;
-   // assign db[15:8] = nrmbp ? 8'bZ : _db_pullup;
-
-   // Note that the RMB instruction only drives the lower 8 bits. On real
-   // hardware, Bus Hold would make the MSB equal to the last memory operation,
-   // which will almost certainly be an instruction fetch, so &54 would be
-   // returned. We do this here by adding an MSB buffer, which SHOULD NOT be
-   // present in real hardware.
-   buffer_541 buf_db_simulator_only (.noe1(niombr), .noe2(nr), .a(8'h54), .y(db[15:8]));
+   flipflop_74h ff_idx(.d(1'b1), .clk(clk2), .nset(nir_idx), .nrst(nrsthold), 
+			.q(nir_idxr));
 
    ///////////////////////////////////////////////////////////////////////////////
    //
@@ -420,14 +306,14 @@ module mbu (nreset,
    wire [7:0] 	mb[7:0];
    wire [7:0] 	mb0, mb1, mb2, mb3, mb4, mb5, mb6, mb7;
 
-   assign mb[0] = { rf_0hi.q0[0], rf_0lo.q0[0] };
-   assign mb[1] = { rf_0hi.q0[1], rf_0lo.q0[1] };
-   assign mb[2] = { rf_0hi.q0[2], rf_0lo.q0[2] };
-   assign mb[3] = { rf_0hi.q0[3], rf_0lo.q0[3] };
-   assign mb[4] = { rf_1hi.q0[0], rf_1lo.q0[0] };
-   assign mb[5] = { rf_1hi.q0[1], rf_1lo.q0[1] };
-   assign mb[6] = { rf_1hi.q0[2], rf_1lo.q0[2] };
-   assign mb[7] = { rf_1hi.q0[3], rf_1lo.q0[3] };
+   assign mb[0] = regfile_mbu.mem[{ctx, 3'd0 }];
+   assign mb[1] = regfile_mbu.mem[{ctx, 3'd1 }];
+   assign mb[2] = regfile_mbu.mem[{ctx, 3'd2 }];
+   assign mb[3] = regfile_mbu.mem[{ctx, 3'd3 }];
+   assign mb[4] = regfile_mbu.mem[{ctx, 3'd4 }];
+   assign mb[5] = regfile_mbu.mem[{ctx, 3'd5 }];
+   assign mb[6] = regfile_mbu.mem[{ctx, 3'd6 }];
+   assign mb[7] = regfile_mbu.mem[{ctx, 3'd6 }];
 
    assign mb0 = mb[0];
    assign mb1 = mb[1];
---
+++ verilog/mbu_control_gal_tb.v
@@ -0,0 +1,155 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// MNU CONTROL GAL TESTBENCH
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+// REDESIGNED IN 2019
+// USES OK/FAIL OUTPUT
+//
+// mbu_control_gal_tb.v -- MBU Decoder GAL Testbench
+//
+// Copyright © 2011-2021 Alexios Chouchoulas
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+`include "mbu.v"
+`timescale 1ns/10ps
+
+module mbu_control_gal_tb();
+   wire [15:0] ir;
+   wire [1:0]  idx;
+   
+   integer    i;
+   
+   // Initialize all variables
+   initial begin        
+      //$display ("time\t rst oe cs q");	
+      // $monitor ("t: %7d | %b > %b %b", $time, raddr, waddir, idx);
+      $dumpfile ("vcd/mbu_control_gal_tb.vcd");
+      $dumpvars (0, mbu_control_gal_tb);
+
+      i = 0;
+      #250;
+      for (i = 0; i < 4096; i = i + 1) begin
+	 #63.5;
+      end
+      
+      #500 $display("345 OK");
+      $finish;
+   end // initial begin
+
+   wire       ndis;
+   wire       nrmbp;
+   wire       nrmbn;
+   wire       nwar;
+   wire       nidxen;
+   wire       nwmbp;
+   wire       nwmbn;
+   wire [1:0] raddr1_0;
+   wire [2:0] ir2_0;
+   wire [2:0] a;
+   wire       noe;
+   wire       nwe;
+
+   assign { ndis, nrmbp, nrmbn, nwar, nidxen, nwmbp, nwmbn, raddr1_0, ir2_0 } = i;
+   
+   // Connect DUT to test bench
+   mbu_control_gal mbu_control_gal (
+				    .ndis(ndis),
+				    .nrmbp(nrmbp),
+				    .nrmbn(nrmbn),
+				    .nwar(nwar),
+				    .nidxen(nidxen),
+				    .nwmbp(nwmbp),
+				    .nwmbn(nwmbn),
+				    .raddr1_0(raddr1_0),
+				    .ir2_0(ir2_0),
+				    .a(a),
+				    .noe(noe),
+				    .nwe(nwe)
+				    );
+   
+   // Verify our findings.
+   reg [8191:0] msg, what;
+   reg [4:0] 	vec;
+   
+   always @ (i) begin
+      #30 begin
+	 msg[7:0] = "";		// Use the msg as a flag.
+
+	 // val: nrmbn, nrmbp, nrctx
+   	 casex ({ndis, nrmbp, nrmbn, nwar, nidxen, nwmbp, nwmbn, raddr1_0, ir2_0})
+	   12'b0_?_?_?_?_?_?_??_???: begin vec = 5'b000_1_1; $sformat(what, "should be idle when disabled"); end
+	   12'b1_1_1_1_?_1_1_??_???: begin vec = 5'b000_0_1; $sformat(what, "enabled, read MB0"); end
+	   
+	   12'b1_0_1_1_?_1_1_??_???: begin vec = 5'b000_0_1; $sformat(what, "nread_mbp"); end
+	   
+	   12'b1_1_0_1_?_1_1_??_000: begin vec = 5'b000_0_1; $sformat(what, "nread_mbn, a=0"); end
+	   12'b1_1_0_1_?_1_1_??_001: begin vec = 5'b001_0_1; $sformat(what, "nread_mbn, a=1"); end
+	   12'b1_1_0_1_?_1_1_??_010: begin vec = 5'b010_0_1; $sformat(what, "nread_mbn, a=2"); end
+	   12'b1_1_0_1_?_1_1_??_011: begin vec = 5'b011_0_1; $sformat(what, "nread_mbn, a=3"); end
+	   12'b1_1_0_1_?_1_1_??_100: begin vec = 5'b100_0_1; $sformat(what, "nread_mbn, a=4"); end
+	   12'b1_1_0_1_?_1_1_??_101: begin vec = 5'b101_0_1; $sformat(what, "nread_mbn, a=5"); end
+	   12'b1_1_0_1_?_1_1_??_110: begin vec = 5'b110_0_1; $sformat(what, "nread_mbn, a=6"); end
+	   12'b1_1_0_1_?_1_1_??_111: begin vec = 5'b111_0_1; $sformat(what, "nread_mbn, a=7"); end
+
+	   12'b1_1_1_0_1_1_1_00_???: begin vec = 5'b000_0_1; $sformat(what, "write_ar_mbp, a=0"); end
+	   12'b1_1_1_0_1_1_1_01_???: begin vec = 5'b001_0_1; $sformat(what, "write_ar_mbd, a=1"); end
+	   12'b1_1_1_0_1_1_1_10_???: begin vec = 5'b010_0_1; $sformat(what, "write_ar_mbs, a=2"); end
+	   12'b1_1_1_0_1_1_1_11_???: begin vec = 5'b011_0_1; $sformat(what, "write_ar_mbz, a=3"); end
+
+	   12'b1_1_1_0_0_1_1_??_000: begin vec = 5'b000_0_1; $sformat(what, "read MB0 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_001: begin vec = 5'b001_0_1; $sformat(what, "read MB1 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_010: begin vec = 5'b010_0_1; $sformat(what, "read MB2 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_011: begin vec = 5'b011_0_1; $sformat(what, "read MB3 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_100: begin vec = 5'b100_0_1; $sformat(what, "read MB4 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_101: begin vec = 5'b101_0_1; $sformat(what, "read MB5 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_110: begin vec = 5'b110_0_1; $sformat(what, "read MB6 index reg"); end
+	   12'b1_1_1_0_0_1_1_??_111: begin vec = 5'b111_0_1; $sformat(what, "read MB7 index reg"); end
+
+	   12'b1_1_1_1_?_0_1_??_???: begin vec = 5'b000_1_0; $sformat(what, "nwrite_mbp"); end
+
+	   12'b1_1_1_1_?_1_0_??_000: begin vec = 5'b000_1_0; $sformat(what, "nwrite_MBN, a=0"); end
+	   12'b1_1_1_1_?_1_0_??_001: begin vec = 5'b001_1_0; $sformat(what, "nwrite_MBN, a=1"); end
+	   12'b1_1_1_1_?_1_0_??_010: begin vec = 5'b010_1_0; $sformat(what, "nwrite_MBN, a=2"); end
+	   12'b1_1_1_1_?_1_0_??_011: begin vec = 5'b011_1_0; $sformat(what, "nwrite_MBN, a=3"); end
+	   12'b1_1_1_1_?_1_0_??_100: begin vec = 5'b100_1_0; $sformat(what, "nwrite_MBN, a=4"); end
+	   12'b1_1_1_1_?_1_0_??_101: begin vec = 5'b101_1_0; $sformat(what, "nwrite_MBN, a=5"); end
+	   12'b1_1_1_1_?_1_0_??_110: begin vec = 5'b110_1_0; $sformat(what, "nwrite_MBN, a=6"); end
+	   12'b1_1_1_1_?_1_0_??_111: begin vec = 5'b111_1_0; $sformat(what, "nwrite_MBN, a=7"); end
+
+	   12'b1_?_?_?_?_?_?_??_???: begin vec = 5'b000_0_1; $sformat(what, "safety interlock, read MB0"); end
+	 endcase // casex ({ waddr, clk4 })
+
+	 if ({a, noe, nwe} !== vec) begin
+   	    $sformat(msg, "failure: %0s", what);
+	 end
+
+	 // Fail if we've logged an issue.
+	 if (msg[7:0]) begin
+	    $display("346 FAIL assertion failed at t=%0d: (%0s)", $time, msg);
+	    $error("assertion failure");
+	    #100 $finish;
+	 end
+	 else $display("345 OK MBU control");
+      end // always @ (raddr)
+   end // always @ (raddr)
+endmodule // mbu_control_gal_tb
+
+
+// End of file.
