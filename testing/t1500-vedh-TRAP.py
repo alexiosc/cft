705dcd21299f905d064eb1fc157babe1fb20cbfa
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-05-09 14:11:48 +0300

    Rewrite TRAP test for context-enabled MBU

--- testing/t1500-vedh-TRAP.py
+++ testing/t1500-vedh-TRAP.py
@@ -30,23 +30,27 @@ def test_TRAP(framework, capsys, tmpdir):
 
     &800100:
     boot:
-            LI &1         ; Set up context 1 (the TRAP context)
-            SCT
-            LI &82
-            SMB mbu.MBP
-            LI &00        ; Configure essential MBRs and enable.
-            SMB mbu.MBZ   ; MBZ=MBS makes reading the stack easier
-            LI &01        ; Configure essential MBRs and enable.
-            SMB mbu.MBS   ; MBZ=MBS makes reading the stack easier
-
             LI &0         ; Set up context 0 (the reset context, and ours)
-            SCT
-            LI &80
-            SMB mbu.MBP
-            LI &00        ; Configure essential MBRs and enable.
-            SMB mbu.MBZ   ; MBZ=MBS makes reading the stack easier
-            LI &01        ; Configure essential MBRs and enable.
-            SMB mbu.MBS   ; MBZ=MBS makes reading the stack easier
+            SCT           ; This is possible because the MBU is still disabled
+            LI &80        ; &80 is this bank.
+            SMB mbu.MBP   ; This enables the MBU, so we must immediately set it up.
+            LI &80        ; 
+            SMB mbu.MBD   ; 
+            LI &01        ; 
+            SMB mbu.MBS   ; 
+            LI &00        ; 
+            SMB mbu.MBZ   ; 
+
+            LI &1
+            TAD
+            LI &82        ; Set up context 1 (the TRAP context)
+            NMB mbu.MBP   ; MBP is &82
+            LMB mbu.MBD   ; Share the other banks with this context
+            NMB mbu.MBD
+            LMB mbu.MBS
+            NMB mbu.MBS
+            LMB mbu.MBZ
+            NMB mbu.MBZ
 
             LI &42
             dfp.PRINTH
@@ -56,12 +60,18 @@ def test_TRAP(framework, capsys, tmpdir):
             HALT
 
     &820000:
-            NOP           ; Reset vector
+            JMP ctxinit   ; Context init vector
             NOP           ; 
             LJMP 4        ; 00:0002: Soft interrupts
             HALT          ; 00:0003: Don't allow hardware interrupts.
             .data thr     ; 00:0004: Trap Handler Routine Address
             .data &82     ; 00:0005: THR memory bank
+
+   ctxinit: LI &00        ; Configure essential MBRs and enable.
+            SMB mbu.MBZ   ; MBZ=MBS makes reading the stack easier
+            LI &01        ; Configure essential MBRs and enable.
+            SMB mbu.MBS   ; MBZ=MBS makes reading the stack easier
+
 
     &821000:
     thr:    dfp.PRINTH
@@ -80,7 +90,7 @@ def test_TRAP(framework, capsys, tmpdir):
                               [ 340, "PRINTH", "0042" ],
                               SUCCESS,
                               HALTED ])
-    result = run_on_framework(framework, capsys, tmpdir, source, rom_addr=8192, timeout=10000, long=True)
+    result = run_on_framework(framework, capsys, tmpdir, source, rom_addr=8192, timeout=1000000, long=True)
     # pprint.pprint(list(result))
     # assert False
     result = list(expected.prepare(result))
