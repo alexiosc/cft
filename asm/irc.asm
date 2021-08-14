88767304c02bca93b824a6c50925f0c28553c9b6
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-08-14 18:56:34 +0300

    Update to current HW implementation

--- asm/irc.asm
+++ asm/irc.asm
@@ -7,17 +7,25 @@
 
 .pushns irc
 
+;;; Definitions
+
+.equ IRC R 0				; Base address
+.equ IER IRC 0				; rw Interrupt Enable Register
+.equ IDR IRC 8				; -w Interrupt Disable Register
+.equ ISR IRC 8				; r- Interrupt Status Register
+
+.equ IRQ_DISABLE IRC 0
+.equ IRQ_ENABLE IRC 8
+
 ;;; Useful macros. You probably won't have to use anything other than these, ISR
 ;;; and ISR_IRQn for testing if an interrupt has been signaled.
 
 .macro enable(irq)
-		LI irc.ICR_EN irc.ICR_IRQ%irq
-		OUT irc.ICR
+		OUT irc.IER %irq
 .endmacro
 
 .macro disable(irq)
-		LI irc.ICR_DIS irc.ICR_IRQ%irq
-		OUT irc.ICR
+		OUT irc.IDR %irq
 .endmacro
 
 .macro ack(irq)
@@ -25,19 +33,11 @@
 		irc.enable(%irq)
 .endmacro
 
-;;; Definitions
-
-.equ IRC &10				; Base address
-	
-.equ IER IRC				; IRC IER (read/write)
-.equ ISR IRC 2				; IRC ISR (read-only)
-
-;;; To detect the IRC, read the IER and/or ISR. The top eight bits should be as
-;;; follows:
+;;; To detect the IRC, read the IER and/or ISR and AND it with DET_MASK. The top
+;;; eight bits should be equal to DET_VAL.
 
 .equ DET_MASK   #11111111'00000000
-.equ ISR_DETECT #00111110'--------
-.equ IER_DETECT #00111101'--------
+.equ DET_VAL    #00111111'--------
 	
 ;; To test for an interrupt, read from the ISR and test one of the
 ;; following bits.
@@ -50,26 +50,6 @@
 .equ ISR_IRQ5 #--1-----			; IRQ5 in ISR
 .equ ISR_IRQ6 #-1------			; IRQ6 in ISR
 .equ ISR_IRQ7 #1-------			; IRQ7 in ISR
-
-;; To enable or mask an interrupt, OR the EN or DIS value with the right
-;; interrupt pattern below. Example:
-;;
-;; LI IRQEN IER_IRQ1
-;; OUT IRC_IER
-;;
-;; To acknowledge an interrupt, disable it. Re-enable it immediately after if
-;; still needed.
-
-.equ ICR_EN   #---'1	                ; ISR enable interrupt bit
-.equ ICR_DIS  #---'0	                ; ISR disable/acknowledge interrupt bit
-.equ ICR_IRQ0 #000'-	                ; IRQ0 in ICR
-.equ ICR_IRQ1 #001'-	                ; IRQ1 in ICR
-.equ ICR_IRQ2 #010'-			; IRQ2 in ICR
-.equ ICR_IRQ3 #011'-			; IRQ3 in ICR
-.equ ICR_IRQ4 #100'-			; IRQ4 in ICR
-.equ ICR_IRQ5 #101'-			; IRQ5 in ICR
-.equ ICR_IRQ6 #110'-			; IRQ6 in ICR
-.equ ICR_IRQ7 #111'-			; IRQ7 in ICR
 
 .popns irc
 
