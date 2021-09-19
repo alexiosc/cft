aa2af32fcbd104f29fca3b9bc41ffb7c94bf5265
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-09-19 13:09:07 +0300

    Prepare processor and peripheral backplane r2134

---
+++ kicad/cft/cft-backplane/Plots/2134/makezip-peripheral.sh
@@ -0,0 +1,1 @@
+zip -9 alexios_cft-backplane-peripheral.zip cft-backplane-peripheral-[FB]_{Cu,Mask,SilkS}.g?? *peripheral*Edge* *peripheral*drl
--- kicad/cft/cft-backplane/Plots/2134/makezip.sh
+++
@@ -1,1 +0,0 @@
-zip -9 alexios_cft-backplane-processor_2134a.zip cft-backplane-processor-[FB]_{Cu,Mask,SilkS}.g?? *Edge* *drl
