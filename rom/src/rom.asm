61aff51f47711050ea393fb3e5f79def6e685a13
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-09-23 20:14:47 +0300

    Add trivial hello world ROM.

--- rom/src/rom.asm
+++ rom/src/rom.asm
@@ -1,11 +1,31 @@
 ; -*- cftasm -*_
 
 .include "asm/dfp2.asm"
+.include "asm/regs.asm"
+.include "asm/mbu.asm"
 
 boot:
+		LI         0
+		SCT
+                LI         &80
+		SMB        mbu.MBP	; Enable MBU, program in ROM
+		SMB        mbu.MBD	; Data in ROM
+		LI         0
+		SMB        mbu.MBZ	; Zero page at start of RAM
+		SMB        mbu.MBS	; Also stack.
+
 		SUCCESS
+		LIA        hello
+		STORE R    B1AI0
+loop:		LOAD I R   B1AI0
 		dfp.PRINTC
-		HALT
+		SNZ
+		JMP end
+		JMP loop
+		
+end:		HALT
 		JMP @
+
+hello:		.str "Hello, world!" 0
 
 ; End of file.
