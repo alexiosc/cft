7334a721c6d681b3f8010c686b23b50087bef814
Alexios Chouchoulas <alexios@bedroomlan.org>
2021-10-10 16:53:36 +0300

    Add script to regenerate BOM

---
+++ kicad/cft/cft-board0/generate-bom.sh
@@ -0,0 +1,1 @@
+~/.kicad_plugins/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --dark-mode --highlight-pin1 --include-tracks --include-nets --blacklist "G***,REF**" cft-board0.kicad_pcb
