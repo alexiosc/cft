ee151e4b041529ad3e5b2dec5760a6e09255921e
Alexios Chouchoulas <alexios@bedroomlan.org>
2022-02-26 15:15:21 +0200

    Update BOM for Board 1

---
+++ kicad/cft/cft-board1/generate-bom.sh
@@ -0,0 +1,1 @@
+~/.kicad_plugins/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --dark-mode --highlight-pin1 --include-tracks --include-nets --blacklist "G***,REF**" cft-board1.kicad_pcb
